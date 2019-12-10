b1810487e19ac1612a853880376b3161571cf053
GAierken <g.aierken@gmail.com>
2019-12-10 11:01:47 -0500

    Done.

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ function handleClick(e) {
 }
 
 /* End Given Code, don't edit above here...*/
+ const greet = (string) => { 
+  let hour = string.split(":")[0]
+   if (hour < 12){return "Good Morning"}
+   if (hour < 17){return "Good Afternoon"}
+   else {return "Good Evening"}
+}
 
-/* Write your implementation of greet() */
+
 /* Write your implementation of displayMessage() */
+ const displayMessage = (string) => {
+     document.getElementById("greeting").innerText = string;    
+}
