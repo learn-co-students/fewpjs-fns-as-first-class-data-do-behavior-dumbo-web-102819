3720866db7c65dce09cc249311ff271804a2fc8f
Griffin Poole <griffinpoole5@gmail.com>
2019-12-10 09:37:21 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,14 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string) {
+  let num = Number(string.replace(":", ","));
+  if (num < 12){
+    return "Good Morning";
+  } else if (num >= 12 && num <= 5) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
