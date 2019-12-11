453207447110e33292f26b494eadcc4052cf2b8f
fanzhong <fanzhongzeng78@gmail.com>
2019-12-11 10:47:01 -0500

    Done.

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function displayMessage(text){
+	document.getElementById("greeting").innerText= text;
+}
+
+function greet(timeString){
+	let time = timeString.split(":");
+	const hour = time[0];
+
+	if (hour < 12)
+	{
+		return "Good Morning";
+	}
+	else if (hour < 17)
+	{
+		return "Good Afternoon";
+	}
+	else
+	{
+		return "Good Evening";
+	}
+}
